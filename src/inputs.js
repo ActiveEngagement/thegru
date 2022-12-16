7d6139dcc14256a49bc8863e6aa516c2717a1aa8
Jacob Lockard <jacoblockard99@gmail.com>
2022-12-16 11:15:12 -0500

    Fix typo

--- src/inputs.js
+++ src/inputs.js
@@ -54,7 +54,7 @@ export default function(getCoreInput) {
         userToken: input('user_token').required().get(),
         filePath: input('file_path').required().get(),
         cardTitle: input('card_title').required().get(),
-        collectionId: input('collection_id').required().get,
+        collectionId: input('collection_id').required().get(),
         boardId: input('board_id').get(),
         boardSectionId: input('board_section_id').get(),
         cardFooter: input('card_footer').get(),
