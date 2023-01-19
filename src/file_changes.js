e7560babff96fbd461058602573a9e28f6d4fffd
Jacob Lockard <jacoblockard99@gmail.com>
2023-01-19 17:00:37 -0500

    Try this

--- src/file_changes.js
+++ src/file_changes.js
@@ -4,7 +4,7 @@ import github from '@actions/github';
 export async function getChangedFiles() {
     let output = '';
 
-    await exec.exec(`git diff --name-only ${github.event.before} ${github.event.after}`, {
+    await exec.exec(`git diff --name-only ${github.context.payload.event.before} ${github.context.payload.event.after}`, {
         listeners: {
             stdout: (data) => {
                 output += data.toString();
