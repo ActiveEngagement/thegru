a10ea98fb215d427c0ac70ff32f0bb58e6e6e6db
Jacob Lockard <jacoblockard99@gmail.com>
2023-01-26 15:19:04 -0500

    Better error

--- src/file_changes.js
+++ src/file_changes.js
@@ -1,15 +1,17 @@
 import exec from '@actions/exec';
+import { TheGuruError } from './error';
 
 export default async function getChangedFiles(github) {
-    let output = '';
-
-    await exec.exec(`git diff --name-only ${github.event.before} ${github.event.after}`, undefined, {
-        listeners: {
-            stdout: (data) => {
-                output += data.toString();
-            }
+    const { exitCode, stdout, stderr } = await exec.getExecOutput(`git diff --name-only ${github.event.before} ${github.event.after}`);
+    
+    if (exitCode === 0) {
+        return stdout.split('\n');
+    }
+    else {
+        if (stderr.includes('bad object')) {
+            throw new TheGuruError('The git command used to determine which files have changed reported a bad commit. Most likely, you did not set `fetch-depth: 0` in your checkout action.')
+        } else {
+            throw new TheGuruError('The git command used to determine which files have changed reported an error!');
         }
-    });
-
-    return output.split('\n');
+    }
 }
