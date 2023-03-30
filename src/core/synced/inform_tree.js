ba1ec4f3baa1c6fa739d45780628c880182e8fcd
Jacob Lockard <jacoblockard99@gmail.com>
2023-03-30 14:56:41 -0400

    Ignore invalid info

--- src/core/synced/inform_tree.js
+++ src/core/synced/inform_tree.js
@@ -5,6 +5,9 @@ import { traverse } from './tree_util.js';
 import matter from 'gray-matter';
 import yaml from 'js-yaml';
 
+const allowedCardInfo = ['title', 'externalUrl'];
+const allowedContainerInfo = ['title', 'description', 'externalUrl'];
+
 /**
  * Traverses the given tree, processes any card content, attempts to attach relevant info to each node. Info is derived
  * from file paths, card content, container info files, and card info files.
@@ -19,6 +22,7 @@ export default function(tree, options) {
         if(node.type === 'card') {
             // We'll read from the frontmatter if it exists and save the content for later.
             const { data, content } = matter(readFileSync(node.file));
+
             Object.assign(node.info, data);
             node.content = content;
 
@@ -28,6 +32,13 @@ export default function(tree, options) {
             if(infoPath) {
                 Object.assign(node.info, yaml.load(readFileSync(infoPath)));
             }
+
+            for (const key of Object.keys(node.info)) {
+                if (!allowedCardInfo.includes(key)) {
+                    delete node.info[key];
+                    logger.warning(`Card "${node.path}" contains invalid info key "${key}. It will be ignored.`);
+                }
+            }
         }
         else if(node.type === 'container') {
             if(node.file) {
@@ -36,6 +47,13 @@ export default function(tree, options) {
                 const infoPath = [infoBase + '.yaml', infoBase + '.yml'].find(p => fs.existsSync(p));
                 if(infoPath) {
                     Object.assign(node.info, yaml.load(readFileSync(infoPath)));
+                }
+            }
+
+            for (const key of Object.keys(node.info)) {
+                if (!allowedContainerInfo.includes(key)) {
+                    delete node.info[key];
+                    logger.warning(`Container "${node.path}" contains invalid info key "${key}. It will be ignored.`);
                 }
             }
         }
